2e42c33a7251ee8eb8f451be2491ca8d5eb71920
Danilo Requena <danilo.requena26@gmail.com>
2020-07-14 13:19:58 -0300

    Initial Commit

---
+++ chooseAvengersORStarWars/chooseAvengersORStarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  chooseAvengersORStarWarsApp.swift
+//  chooseAvengersORStarWars
+//
+//  Created by Danilo Requena on 7/14/20.
+//
+
+import SwiftUI
+
+@main
+struct chooseAvengersORStarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
